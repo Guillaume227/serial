c51783b1a01d0ec2a29a3681d864ea6662e03854
Guillaume Giraud <guillaume.giraud@gmail.com>
2022-12-16 17:32:29 +0100

    [unix] added a check that the enumerated ports can be opened to filter out dummy results

--- src/impl/list_ports/list_ports_linux.cc
+++ src/impl/list_ports/list_ports_linux.cc
@@ -21,6 +21,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h> // Contains file controls like O_RDWR
 
 #include "serial/serial.h"
 
@@ -293,7 +294,15 @@ usb_sysfs_hw_string(const string& sysfs_path)
 
     return format("USB VID:PID=%s:%s %s", vid.c_str(), pid.c_str(), serial_number.c_str() );
 }
-
+namespace {
+bool check_exists(std::string const& port_name) {
+    auto _fd = open(port_name.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
+    bool failed_open = _fd != -1;
+    if (not failed_open) {
+        close(_fd);
+    }
+}
+}
 vector<PortInfo>
 serial::list_ports()
 {
@@ -314,6 +323,10 @@ serial::list_ports()
     while( iter != devices_found.end() )
     {
         string device = *iter++;
+
+        if (not check_exists(device)) {
+            continue;
+        }
 
         vector<string> sysfs_info = get_sysfs_info( device );
 
